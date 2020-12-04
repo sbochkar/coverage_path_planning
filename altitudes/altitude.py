e13c6adde90042c73f970d667c383eb0ab60b576
Stanislav Bochkarev <stas7775@gmail.com>
2020-12-03 20:47:14 -0500

    [TDD] Finished implementation and tests for altitude.

--- altitudes/altitude.py
+++ altitudes/altitude.py
@@ -1,9 +1,11 @@
 """Module for computing altitude of polygons."""
-from typing import Dict, Tuple, List
+from math import atan2, pi
+from itertools import tee
+from typing import Tuple, List, Set
 import sys
 
-from shapely.geometry import Polygon, Point
-from shapely.affinity import rotate
+from shapely.geometry import Polygon, Point, LineString
+from shapely.affinity import rotate, translate
 
 
 def get_min_altitude(polygon: Polygon):
@@ -15,7 +17,7 @@ def get_min_altitude(polygon: Polygon):
 
     """
     min_alt = sys.float_info.max
-    min_dir = 0
+    min_dir = 0.
     for theta in get_directions_set(polygon):
         test_alt = get_altitude(polygon, theta)
         if test_alt <= min_alt:
@@ -23,6 +25,26 @@ def get_min_altitude(polygon: Polygon):
             min_dir = theta
 
     return min_alt, min_dir
+
+
+def get_directions_set(polygon: Polygon) -> Set[float]:
+    """Generate a list of directions orthogonal to edges of polygon
+
+    Args:
+        polygon (Polygon): Polygon as a shapely object.
+    Returns:
+        dirs (Set): Set of directions in degrees.
+    """
+    dirs: Set[float] = set()
+    for chain in [polygon.exterior, *polygon.interiors]:
+        for idx, coord in enumerate(chain.coords[:-1]):
+            edge = LineString([coord, chain.coords[idx + 1]])
+            edge = rotate(edge, 90, origin=Point(coord))  # Perpendicular to the edge.
+            edge = translate(edge, xoff=-coord[0], yoff=-coord[1])  # Direction vector.
+            deg = atan2(edge.coords[1][1], edge.coords[1][0]) % pi
+            dirs.add(180 * deg / pi)
+
+    return dirs
 
 
 def get_altitude(polygon: Polygon, theta: float) -> float:
@@ -44,100 +66,41 @@ def get_altitude(polygon: Polygon, theta: float) -> float:
     # Align direction with x axis, for convenience.
     polygon = rotate(polygon, -theta)
 
-    # Prepend last coordinate for easier lookups for connecting verts at first element or a ring.
-    chains = [[chain.coords[-2], *chain.coords] for chain in [polygon.exterior, *polygon.interiors]]
+    prevs: List[Tuple[float, float]] = []
+    currs: List[Tuple[float, float]] = []
+    nexts: List[Tuple[float, float]] = []
+    for chain in [polygon.exterior, *polygon.interiors]:
+        cur, nxt = tee(chain.coords)
+        next(nxt, None)
+        for cur_vert, nxt_vert in zip(cur, nxt):
+            if cur_vert[0] != nxt_vert[0]:
+                currs.append(cur_vert)
+                nexts.append(nxt_vert)
 
-    event_map = {}
-    for chain in chains:
-        for i in range(1, len(chain) - 1):
-            if chain[i][0] == chain[i - 1][0]:
-                event_map[chain[i]] = [chain[i - 2], chain[i + 1]]
-                if event_map.get(chain[i - 1], None):
-                    del event_map[chain[i - 1]]
-            else:
-                event_map[chain[i]] = [chain[i - 1], chain[i + 1]]
+        cur, nxt = tee(chain.coords[::-1])
+        next(nxt, None)
+        for cur_vert, nxt_vert in zip(cur, nxt):
+            if cur_vert[0] != nxt_vert[0]:
+                prevs.append(nxt_vert)
+        prevs = prevs[1:] + [prevs[0]]
+        prevs = prevs[::-1]
 
-    from pprint import pprint
-    pprint(event_map)
-    
+    event_map = list(zip(prevs, currs, nexts))
+    sorted_by_x = sorted(event_map, key=lambda point: point[1][0])
 
-
-    # Iterate over all points in the polygon. Hashed by x coordinate only.
-    def get_neighbor_map_shp(polygon: Polygon) -> Dict[Point, Point]:
-        """This function will form an adjacency list representing polygon's edges.
-
-        Args:
-            polygon (Polygon): Shapely object representing the polygon.
-
-        Returns:
-            neighbors_map (Dict): A map between vertex and its two neighbors.
-        """
-        neighbors_map: Dict[Tuple[float, float], Tuple[Tuple[float, float]]] = {}
-        for chain in [polygon.exterior, *polygon.interiors]:
-            for idx, coord in list(enumerate(chain.coords))[:-1]:
-                neighbors_map[coord] = (chain.coords[idx - (1 if idx != 0 else 2)],
-                                        chain.coords[idx + 1])
-        return neighbors_map
-
-    neighbors_map = get_neighbor_map_shp(polygon)
-
-    # sorted_by_x = sorted(neighbors_map.keys(), key=lambda point: point[0])
-    sorted_by_x = sorted(event_map.keys(), key=lambda point: point[0])
-
-    altitude = 0.
+    altitude, prev_x = 0., 0.
     active_event_counter = 0
-    prev_x = -sys.float_info.max
-    checked_verts = []
-    for i, vert in enumerate(sorted_by_x):
+    for (prv_x, _), (cur_x, _), (nxt_x, _) in sorted_by_x:
 
-        if vert in checked_verts:
-            continue
-
-        v_x, _ = vert
-        (adj_x_1, _), (adj_x_2, _) = neighbors_map[vert]
-
-        if i > 0:
-            deltax = v_x - prev_x
+        if active_event_counter > 0:
+            deltax = cur_x - prev_x
             altitude += active_event_counter * deltax
-        prev_x = v_x
 
-        if (adj_x_1 > v_x) and (adj_x_2 > v_x):
+        if prv_x > cur_x < nxt_x:
             active_event_counter += 1
-        elif (adj_x_1 < v_x) and (adj_x_2 < v_x):
+        elif prv_x < cur_x > nxt_x:
             active_event_counter -= 1
 
-        if (adj_x_1 > v_x) and (adj_x_2 == v_x):
-            if resolve_local_equality(neighbors_map, neighbors_map[vert][1], vert) == 1:
-                active_event_counter += 1
-                checked_verts.append(neighbors_map[vert][1])
-        if (adj_x_2 > v_x) and (adj_x_1 == v_x):
-            if resolve_local_equality(neighbors_map, neighbors_map[vert][0], vert) == 1:
-                active_event_counter += 1
-                checked_verts.append(neighbors_map[vert][0])
-        if (adj_x_1 < v_x) and (adj_x_2 == v_x):
-            if resolve_local_equality(neighbors_map, neighbors_map[vert][1], vert) == -1:
-                active_event_counter -= 1
-                checked_verts.append(neighbors_map[vert][1])
-        if (adj_x_2 < v_x) and (adj_x_1 == v_x):
-            if resolve_local_equality(neighbors_map, neighbors_map[vert][0], vert) == -1:
-                active_event_counter -= 1
-                checked_verts.append(neighbors_map[vert][0])
+        prev_x = cur_x
 
     return altitude
-
-
-def resolve_local_equality(neighbors_map: Dict[Tuple[float, float], Tuple[Tuple[float, float]]],
-                           curr_vert: Tuple[float, float], prev_vert: Tuple[float, float]):
-    """Recursivly determine the action in the case where adjacent edges are collinear."""
-    adjacent_verts = list(neighbors_map[curr_vert])
-    if prev_vert in adjacent_verts:
-        adjacent_verts.remove(prev_vert)
-
-    x_curr, _ = curr_vert
-    x_next, _ = adjacent_verts[0]
-
-    if x_next > x_curr:
-        return 1
-    if x_next < x_curr:
-        return -1
-    return resolve_local_equality(neighbors_map, adjacent_verts[0], curr_vert)
--- altitudes/test_altitude.py
+++ altitudes/test_altitude.py
@@ -7,10 +7,11 @@ import pytest
 
 from shapely.geometry import Polygon
 
-from altitude import get_altitude
+from altitude import get_altitude, get_min_altitude
 
 
 UNIT_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)]]
+RECTANGLE = [[(0., 0.), (8., 0.), (8., 1.), (0., 1.)]]
 UNIT_HOLE_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
                     [[(0.1, 0.1), (0.1, 0.9), (0.9, 0.9), (0.9, 0.1)]]]
 UNIT_HOLE_PLUR_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
@@ -21,11 +22,12 @@ UNIT_TRICK_HOLE_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
                             (0.2, 0.4), (0.2, 0.8), (0.1, 0.8), (0.1, 0.9),
                             (0.9, 0.9), (0.9, 0.1)]]]
 UNIT_DEAD_END_HOLE_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
-                             [[(0.1, 0.1), (0.5, 0.2), (0.1, 0.9), (0.9, 0.9), (0.9, 0.1)]]]
+                             [[(0.1, 0.1), (0.2, 0.2), (0.1, 0.9), (0.9, 0.9), (0.9, 0.1)]]]
 UNIT_DEAD_END_BOTH_SIDES_HOLE_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
-                                        [[(0.1, 0.1), (0.5, 0.2), (0.1, 0.9), (0.9, 0.9),
+                                        [[(0.1, 0.1), (0.2, 0.2), (0.1, 0.9), (0.9, 0.9),
                                           (0.8, 0.5), (0.9, 0.1)]]]
 BOOMERANG = [[(0., 1.), (2., 0.), (1., 1.), (2., 2.)]]
+CLAMP = [[(0., 0.), (0., 10.), (4., 10.), (4., 0.), (3., 0.), (3., 9.), (1., 9.), (1., 0.)]]
 
 
 @pytest.mark.parametrize('test_polygon, test_theta, exp_alt', [
@@ -39,15 +41,40 @@ BOOMERANG = [[(0., 1.), (2., 0.), (1., 1.), (2., 2.)]]
      0., 0.2 + 2 * 0.8),
     (UNIT_TRICK_HOLE_SQUARE,
      0., 0.1 + 0.3 + 2 * 0.7 + 0.1),
-    # (UNIT_DEAD_END_HOLE_SQUARE,
-    #  0., 0.1 + 0.3 + 2 * 0.7 + 0.1),
-    # (UNIT_DEAD_END_BOTH_SIDES_HOLE_SQUARE,
-    #  0., 0.1 + 0.3 + 2 * 0.6 + 0.3 + 0.1),
-    # (UNIT_HOLE_PLUR_SQUARE,
-    #  0., 0.2 + 2 * 0.8),
+    (UNIT_DEAD_END_HOLE_SQUARE,
+     0., 0.1 + 0.3 + 2 * 0.7 + 0.1),
+    (UNIT_DEAD_END_BOTH_SIDES_HOLE_SQUARE,
+     0., 0.1 + 0.3 + 2 * 0.6 + 0.3 + 0.1),
+    (UNIT_HOLE_PLUR_SQUARE,
+     0., 0.2 + 2 * 0.8),
+    ([[(0.0, 0.0), (0.1, 0.0), (0.2, 0.0), (0.3, 0.0), (1.0, 0.0), (1.0, 1.0), (0., 1.0)]],
+     0, 1.),
+    ([[(0.0, 0.0), (1.0, 0.0), (1.0, 0.1), (1.0, 0.1), (1.0, 0.1), (1.0, 0.1), (1.0, 1.0),
+       (0., 1.0)]],
+     0, 1.),
+    ([[(0.0, 0.0), (1.0, 0.0), (1.0, 0.1), (1.0, 0.2), (1.0, 0.3), (1.0, 0.4), (1.0, 1.0),
+       (0., 1.0)]],
+     0, 1.),
+    ([[(0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0., 1.0), (0., 0.9), (0., 0.8), (0., 0.7), (0., 0.6)]],
+     0, 1.),
+    ([[(0.0, 0.2), (0.0, 0.1), (0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0., 1.0), (0., 0.9), (0., 0.8),
+       (0., 0.7), (0., 0.6), (0., 1.0)]],
+     0, 1.),
 ])
 def test_get_altitude(test_polygon, test_theta, exp_alt):
     """Verifies that altitude of polygons is computed correctly."""
     result = get_altitude(Polygon(*test_polygon), test_theta)
 
     assert isclose(result, exp_alt)
+
+
+@pytest.mark.parametrize('test_polygon, exp_alt, exp_dir', [
+    (RECTANGLE, 1., 90.),
+    (CLAMP, 4., 0.),
+
+])
+def test_get_min_altitude(test_polygon, exp_alt, exp_dir):
+    """Verifies that minimum altitude is found."""
+    min_alt, min_dir = get_min_altitude(Polygon(*test_polygon))
+
+    assert (min_alt, min_dir) == (exp_alt, exp_dir)
