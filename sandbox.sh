c1bcd870110ed620cfe23ea187295fc2dd824369
Stanislav Bochkarev <stas7775@gmail.com>
2020-12-18 17:53:25 -0500

    [TDD] Started work on sampling.

--- sandbox.sh
+++ sandbox.sh
@@ -4,6 +4,7 @@ xhost +
 
 docker run --rm -it \
     -e DISPLAY=$DISPLAY \
+    -e PYTHONPATH=/workspace \
     --net=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $PWD:/workspace -w /workspace ${IMAGE} bash
