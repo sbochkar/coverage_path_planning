87c28128aba35acc7250be75af0b2b704cdff518
Stanislav Bochkarev <stas7775@gmail.com>
2020-12-29 21:22:32 -0500

    [TDD] Started work on optimizer section.

---
+++ optimizer/min_alt_optimizer.py
@@ -0,0 +1,319 @@
+"""Module for minimum altitude decomposition of polygons."""
+from typing import List, Tuple
+
+from numpy import linspace
+from shapely.geometry import LineString, LinearRing, Polygon, MultiLineString
+
+
+from decomposition.decomposition import Decomposition
+
+# from pkg.aux.altitudes import altitude as alt
+# from pkg.decompositions.min_alt import cuts
+# from pkg.poly_operations.others import chain_combination
+# from pkg.poly_operations.others import reflex
+# from pkg.poly_operations.others import operations
+# from pkg.decompositions import adjacency
+# from pkg.decompositions import edges
+
+
+def is_reflex(prev_vert: Tuple[float, float],
+              mid_vert: Tuple[float, float],
+              next_vert: Tuple[float, float]) -> bool:
+    """Method for checking if three verts are reflex or not.
+
+    Args:
+        prev_vert (Tuple[float]): First vertex in a sequence.
+        mid_vert (Tuple[float]): Second vertex in a sequence.
+        next_vert (Tuple[float]): Third vertex in a sequence.
+
+    Returns:
+        True if the sequence is reflex.
+    """
+    dx_1, dx_2 = mid_vert[0] - next_vert[0], prev_vert[0] - mid_vert[0]
+    dy_1, dy_2 = mid_vert[1] - next_vert[1], prev_vert[1] - mid_vert[1]
+    return dx_1 * dy_2 - dy_1 * dx_2 > 0.0
+
+
+def compute_vertex_sampler(decomposition: Decomposition) -> List[LineString]:
+    """
+    One of the simple samplers that generates cuts that originate at a reflex vertex and end
+    at some vertex of a polygon.
+
+    Args:
+        decomposition (Decomposition): An instance of decomposition.
+
+    Returns:
+        A list of valid cuts.
+    """
+    reflex_verts = get_cut_origins(decomposition)
+    orig_poly = decomposition.orig_polygon
+
+    coords: List[Tuple[float, float]] = []
+    for chain in [orig_poly.exterior, *orig_poly.interiors]:
+        coords.extend(chain.coords[:-1])
+
+    samples = [LineString((x, y)) for x in reflex_verts for y in coords]
+
+    final_list = []
+    for sample in samples:
+        common_pts = orig_poly.intersection(sample)
+
+        if not (common_pts.is_empty or
+                not common_pts.geom_type == 'MultiPoint' or # Intersection should be MultiPoint.
+                len(common_pts) != 2 or # MultiPoint must ONLY have 2 points.
+                not sample.within(orig_poly) or # Split line should be inside cell.
+                any(sample.intersects(hole) for hole in orig_poly.interiors)): # Cut touching holes?
+            final_list.append(sample)
+
+    return final_list
+
+
+def get_cut_origins(decomposition: Decomposition) -> List[Tuple[float, float]]:
+    """Find all reflex vertecies in the polygon.
+
+    Args:
+        decomposition (Decomposition): Instance of a decomposition.
+
+    Returns:
+        List of reflex vertices in the decomposition.
+    """
+    reflex_verts: List[Tuple[float, float]] = []
+    for cell in decomposition.cells:
+        for chain in [cell.exterior, *cell.interiors]:
+            points = chain.coords[:-1]
+            for i, _ in enumerate(points):
+                if is_reflex((points[i - 1]), points[i], points[(i + 1) % len(points)]):
+                    reflex_verts.append(points[i])
+    return reflex_verts
+
+
+# def get_first_shared_edge(v, adj):
+#     for i in range(len(adj)):
+#         for j in range(i, len(adj)):
+#             if not adj[i][j] is None:
+#                 if v[1] in adj[i][j]:
+#                     return (i, j, adj[i][j])
+#     return []
+
+
+# def combine_polygons_from_decomposition(v, decomposition):
+
+#     # Build a set of all shared edges in the decomposition which shared v
+#     # TODO: This does not create a copy....
+#     new_decomposition = decomposition
+#     adj = adjacency.get_adjacency_as_matrix(new_decomposition)
+#     shared_edge_tuple = get_first_shared_edge(v, adj)
+
+#     while shared_edge_tuple:
+#         p1_id, p2_id, test_edge = shared_edge_tuple
+#         P1 = new_decomposition[p1_id][0]
+#         P2 = new_decomposition[p2_id][0]
+
+
+#         # Combine the two into one polygon
+#         P = operations.combine_two_adjacent_polys(P1, P2, test_edge)
+#         # Remove P1 and P2 from decomposition set
+#         if p1_id > p2_id: new_decomposition.pop(p1_id); new_decomposition.pop(p2_id)
+#         else: new_decomposition.pop(p2_id); new_decomposition.pop(p1_id)
+#         # Insert the new polygon in the decomposition, assuming no new holes
+#         new_decomposition.append([P, []])
+#         adj = adjacency.get_adjacency_as_matrix(new_decomposition)
+#         shared_edge_tuple = get_first_shared_edge(v, adj)
+
+#     return new_decomposition
+
+
+
+# def get_polygon_containing_point(decomposition, v):
+#     """
+#     """
+
+#     for i in range(len(decomposition)):
+
+#         chain = decomposition[i][0]
+
+#         if v[1] in chain: return decomposition[i], i
+
+
+# def update_v_id(P, v):
+
+#     for i in range(len(P[0])):
+#         if v[1] == P[0][i]: return (i, v[1])
+
+
+# def round_vertecies(p):
+#     new_p = []
+#     for v in p:
+#         new_p.append((round(v[0],5), round(v[1],5)))
+#     return new_p
+
+# def euc_distance(p1, p2):
+#     return sqrt((p2[1]-p1[1])**2+(p2[0]-p1[0])**2)
+
+# def post_processs_decomposition(decomp):
+#     for a in range(len(decomp)):
+#         for b in range(a+1, len(decomp)):
+
+#             p1 = decomp[a][0] #ext only
+#             p2 = decomp[b][0] #ext only
+
+#             p1_new = p1
+#             p2_new = p2
+
+#             n1 = len(p1); n2 = len(p2)
+
+#             # Test every pair of edges from both polygons to see equality
+#             for i in range(n1):
+#                 edge1 = [p1[i]]+[p1[(i+1)%n1]]
+
+#                 for j in range(n2):
+#                     edge2 = [p2[j]]+[p2[(j+1)%n2]]
+
+#                     has_overlap, coords = edges.check_for_overlap(edge1, edge2)
+#                     if has_overlap:
+
+#                         # Consider each edge separately.
+#                         if euc_distance(edge1[0], coords[0])<euc_distance(edge1[0], coords[1]):
+#                             if euc_distance(edge1[0], coords[0]) > 0.001: new_edge1 = [edge1[0]]+[coords[0]]
+#                             else: new_edge1 = [edge1[0]]
+
+#                             if euc_distance(edge1[1], coords[1]) > 0.001: new_edge1 = new_edge1+[coords[1]]+[edge1[1]]
+#                             else: new_edge1 = new_edge1 + [edge1[1]]
+#                         else:
+#                             if euc_distance(edge1[0], coords[1]) > 0.001: new_edge1 = [edge1[0]]+[coords[1]]
+#                             else: new_edge1 = [edge1[0]]
+
+#                             if euc_distance(edge1[1], coords[0]) > 0.001: new_edge1 = new_edge1+[coords[0]]+[edge1[1]]
+#                             else: new_edge1 = new_edge1 + [edge1[1]]
+
+#                         if euc_distance(edge2[0], coords[0])<euc_distance(edge2[0], coords[1]):
+#                             if euc_distance(edge2[0], coords[0]) > 0.001: new_edge2 = [edge2[0]]+[coords[0]]
+#                             else: new_edge2 = [edge2[0]]
+
+#                             if euc_distance(edge2[1], coords[1]) > 0.001: new_edge2 = new_edge2+[coords[1]]+[edge2[1]]
+#                             else: new_edge2 = new_edge2 + [edge2[1]]
+#                         else:
+#                             if euc_distance(edge2[0], coords[1]) > 0.001: new_edge2 = [edge2[0]]+[coords[1]]
+#                             else: new_edge2 = [edge2[0]]
+
+#                             if euc_distance(edge2[1], coords[0]) > 0.001: new_edge2 = new_edge2+[coords[0]]+[edge2[1]]
+#                             else: new_edge2 = new_edge2 + [edge2[1]]
+
+#                         # Now insert new_edges to the polygon
+#                         if len(new_edge1) == 3:
+#                             p1_new.insert(i+1, new_edge1[1])
+#                         elif len(new_edge1) == 4:
+#                             p1_new.insert(i+1, new_edge1[1])
+#                             p1_new.insert(i+2, new_edge1[2])
+
+#                         if len(new_edge2) == 3:
+#                             p2_new.insert(j+1, new_edge2[1])
+#                         elif len(new_edge1) == 4:
+#                             p2_new.insert(j+1, new_edge2[1])
+#                             p2_new.insert(j+2, new_edge2[2])
+
+#                         decomp[a] = [p1_new, []]
+#                         decomp[b] = [p2_new, []]
+
+#     return decomp
+
+# def reoptimize(P, decomposition, adj):
+#     """
+#     Assuming running on convex decomposition
+#     """
+
+#     # Build a set of reflex verticies
+#     new_decomposition = decomposition
+#     R = reflex.find_reflex_vertices(P)
+
+#     while R:
+#         # Pick one reflex vertex from R
+#         v = R.pop()
+
+#         # Combine all polygons in shared_edges to form one polygon
+#         new_decomposition = combine_polygons_from_decomposition(v, new_decomposition)
+#         # v should belong to only one polygon at this point
+#         adj = adjacency.get_adjacency_as_matrix(new_decomposition)
+#         if get_first_shared_edge(v[1], adj):
+#             print("COMBINATION INCOMPLETE?")
+
+#         # Find the polygon containing v[1] and its altitude
+#         P, P_id = get_polygon_containing_point(new_decomposition, v)
+#         altitude_P = alt.get_min_altitude(P)
+
+#         # Update v within P
+#         test_lr = LinearRing(P[0])
+#         if not test_lr.is_ccw: P[0] = P[0][::-1]
+#         v_new = update_v_id(P, v)
+
+#         # Find an optimal cut from v[1] within P
+#         cut = cuts.find_optimal_cut(P, v_new)
+#         # Evaluate the potential optimal cut
+#         if cut and cut is not None: # Not empty
+#             p_l, p_r = cuts.perform_cut(P, [v[1], cut[0]])
+
+#             p_l = round_vertecies(p_l)
+#             p_r = round_vertecies(p_r)
+#             altitude_pl = alt.get_min_altitude([p_l, []])
+#             altitude_pr = alt.get_min_altitude([p_r, []])
+
+#             # If cut improves altitude
+#             if altitude_pr+altitude_pl < altitude_P:
+
+#                 decomposition.pop(P_id)
+#                 decomposition.append([p_l, []])
+#                 decomposition.append([p_r, []])
+
+#                 # Need to process all polygons in the decomposition to introduce
+#                 # 	aditional veritices where cuts were made
+#                 decomposition = post_processs_decomposition(decomposition)
+#     return decomposition
+
+
+# def decompose_temp(polygon: Polygon) -> Any:
+#     """High level call to minimum altitude decomposition.
+
+#     Args:
+#         polygon (Polygon): Shapely object representing polygon.
+#     Returns:
+#         decomposition: A set of polygons.
+#     """
+#     improving_cuts: List[LineString] = []
+#     for reflex_vert in reflex.find_reflex_vertices(polygon):
+#         cut = cuts.find_optimal_cut(polygon, reflex_vert)
+#         if cut:
+#             improving_cuts.append(cut)
+
+#     p_l, p_r = cuts.perform_cut(polygon, [reflex_vert, cut[0]])
+
+#     # TODO: Need to convert candidate_queue into decomposition.
+
+
+# def decompose(polygon: List[List[Any]]) -> Any:
+#     recursive_cuts(polygon)
+
+#     return list_of_polygons
+
+
+# list_of_polygons = []
+# def recursive_cuts(polygon):
+#     """Recursive function for performing re-optimizing cuts on the polygon.
+
+#     TODO: So many things are wrong here.
+
+#     Args:
+#         polygon (List[List[Any]]): Polygon the cannonical form.
+#     """
+#     reflex_verts = reflex.find_reflex_vertices(polygon)
+#     while reflex_verts:
+#         reflex_vert = reflex_verts.pop()
+
+#         cut = cuts.find_optimal_cut(polygon, reflex_vert)
+
+#         if cut:
+#             p_l, p_r = cuts.perform_cut(polygon, [reflex_vert[1], cut[0]])
+
+#             recursive_cuts([p_l, []])
+#             recursive_cuts([p_r, []])
+#             return
+#     list_of_polygons.append(polygon)
---
+++ optimizer/test_min_alt_optimizer.py
@@ -0,0 +1,82 @@
+import pytest
+
+from shapely.geometry import LineString, Polygon
+
+from decomposition.decomposition import Decomposition
+from altitudes.altitude import get_min_altitude
+from min_alt_optimizer import compute_vertex_sampler, get_cut_origins
+
+
+UNIT_SQUARE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)]]
+UNIT_SQUARE_HOLE = [[(0., 0.), (1., 0.), (1., 1.), (0., 1.)],
+                    [[(0.2, 0.2), (0.2, 0.8), (0.8, 0.8), (0.8, 0.2)]]]
+ONE_REFLEX = [[(0., 0.), (1., 0.), (2., 1.), (3., 0.), (4., 0.), (4., 2.), (0., 2.)]]
+ONE_REFLEX_HOLE = [[(0., 0.), (1., 0.), (2., 1.), (3., 0.), (4., 0.), (4., 2.), (0., 2.)]]
+TWO_REFLEX = [[(0., 0.), (1., 0.), (2., 1.), (3., 0.), (4., 0.), (4., 3.), (3., 3.), (2., 2.),
+               (1., 3.), (0., 3.)]]
+
+
+@pytest.mark.parametrize("test_polygon, exp_origins", [
+    (ONE_REFLEX, [(2., 1.)]),
+])
+def test_get_cut_origins(test_polygon, exp_origins):
+    """Verify that cut origins is computed correctly."""
+    decomp = Decomposition(Polygon(*test_polygon))
+
+    origins = get_cut_origins(decomp)
+
+    assert origins == exp_origins
+
+
+@pytest.mark.parametrize("test_polygon, expected_sample_space", [
+    (ONE_REFLEX, [((2., 1.), (0., 0.)),
+                  ((2., 1.), (4., 0.)),
+                  ((2., 1.), (4., 2.)),
+                  ((2., 1.), (0., 2.)),
+                 ]),
+    (TWO_REFLEX, [((2., 1.), (0., 0.)),
+                  ((2., 1.), (4., 0.)),
+                  ((2., 1.), (4., 3.)),
+                  ((2., 1.), (3., 3.)),
+                  ((2., 1.), (2., 2.)),
+                  ((2., 1.), (1., 3.)),
+                  ((2., 1.), (0., 3.)),
+                  ((2., 2.), (0., 0.)),
+                  ((2., 2.), (1., 0.)),
+                  ((2., 2.), (2., 1.)),
+                  ((2., 2.), (3., 0.)),
+                  ((2., 2.), (4., 0.)),
+                  ((2., 2.), (4., 3.)),
+                  ((2., 2.), (0., 3.)),
+                 ]),
+    (UNIT_SQUARE_HOLE, [((0.2, 0.2), (0., 0.)),
+                        ((0.2, 0.2), (0., 1.)),
+                        ((0.2, 0.2), (1., 0.)),
+                        ((0.2, 0.8), (0., 0.)),
+                        ((0.2, 0.8), (0., 1.)),
+                        ((0.2, 0.8), (1., 1.)),
+                        ((0.8, 0.8), (0., 1.)),
+                        ((0.8, 0.8), (1., 1.)),
+                        ((0.8, 0.8), (1., 0.)),
+                        ((0.8, 0.2), (1., 1.)),
+                        ((0.8, 0.2), (1., 0.)),
+                        ((0.8, 0.2), (0., 0.)),
+                       ]),
+])
+def test_vertex_sampler(test_polygon, expected_sample_space):
+    """
+    Unit tests for simple sampler. Simple sampler's objectie is given a polygon, propose a list
+    of cuts which originate from reflex verticies and end on some vertex of the polygon.
+    These cuts will then be used by the optimizer to find the most optimal one.
+    """
+    decomp = Decomposition(Polygon(*test_polygon))
+
+    sample_space = compute_vertex_sampler(decomp)
+
+    ls_expected = [LineString(a) for a in expected_sample_space]
+    for result in sample_space:
+        assert any(result.equals(a) for a in ls_expected)
+
+
+def test_optimize():
+    """Verify that the optimizer attempts to choose the best cut from the supplied cuts."""
