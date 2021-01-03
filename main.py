f818107ddb5494e4df908522bef39077a70a8167
Stanislav Bochkarev <stas7775@gmail.com>
2021-01-03 13:29:22 -0500

    [TDD] Added visualization tools.

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import sys
 from decomposition.decomposition import Decomposition
 from polygons.polygons import polygon_factory
 from optimizer.min_alt_optimizer import min_alt_optimize, vertex_sampler
-from visuals.coverage_plot import plot_decomposition
+from visuals.coverage_plot import plot_decomposition, init_axis, display
 
 
 def main(polygon_id: int):
     """ Main entrypoint for the CPP."""
+    axis = init_axis()
 
     decomposition = Decomposition(polygon_factory(polygon_id))
+    plot_decomposition(axis, decomposition, 1)
+
     samples = vertex_sampler(decomposition)
+
     min_alt_optimize(decomposition, samples)
-    plot_decomposition(decomposition)
+    plot_decomposition(axis, decomposition, 2)
+
+    display()
 
 
 if __name__ == "__main__":
---
+++ visuals/coverage_plot.py
@@ -0,0 +1,41 @@
+"""Module containing various methods for visualizing decomposition."""
+from typing import Any, TYPE_CHECKING
+
+import matplotlib.pyplot as plt
+
+if TYPE_CHECKING:
+    from decomposition import Decomposition
+
+
+def init_axis():
+    """Initializes figure object for plotting."""
+    return plt.figure()
+
+
+def display():
+    """Display the figure."""
+    plt.show()
+
+
+def plot_decomposition(fig: Any, decomposition: 'Decomposition', order_num=1):
+    """Function plots current status of decomposition.
+
+    Args:
+        fig (Any): Figure object from matplotlib that will be used to plot decomposition.
+        decomposition (Decomposition): Instance of decomposition.
+        order_num (int): Order of the subplot on the figure.
+    """
+    axis = fig.add_subplot(2, 1, order_num)
+    plt.axis("equal")
+
+    axis.get_yaxis().set_ticks([])
+    axis.get_xaxis().set_ticks([])
+
+    minx, miny, maxx, maxy = decomposition.orig_polygon.bounds
+
+    for cell in decomposition.cells:
+        axis.plot(*cell.exterior.xy, color='#6699cc', alpha=0.7,
+                  linewidth=3, solid_capstyle='round', zorder=1)
+
+    axis.set_xlim([minx - 0.5, maxx + 0.5])
+    axis.set_ylim([miny - 0.5, maxy + 0.5])
