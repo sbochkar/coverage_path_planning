ad14eaefcf396ebae0160383ae9b89a54c73e26f
Stanislav Bochkarev <stas7775@gmail.com>
2021-01-17 20:32:42 -0500

    [TDD] Add a samples that supports holes.

--- optimizer/min_alt_optimizer.py
+++ optimizer/min_alt_optimizer.py
@@ -1,4 +1,5 @@
 """Module for minimum altitude decomposition of polygons."""
+from itertools import combinations, product
 from typing import List, Tuple
 
 from shapely.geometry import LineString, LinearRing, Polygon, MultiLineString
@@ -43,7 +44,14 @@ def vertex_sampler(decomposition: Decomposition) -> List[LineString]:
     for chain in [orig_poly.exterior, *orig_poly.interiors]:
         coords.extend(chain.coords[:-1])
 
-    samples = [LineString((x, y)) for x in reflex_verts for y in coords]
+    samples_prelim = [LineString((x, y)) for x in reflex_verts for y in coords]
+    samples = []
+    for i, sample in enumerate(samples_prelim):
+        for test_sample in range(i + 1, len(samples_prelim)):
+            if sample.equals(samples_prelim[test_sample]):
+                break
+        else:
+            samples.append(sample)
 
     final_list = []
     for sample in samples:
@@ -59,6 +67,49 @@ def vertex_sampler(decomposition: Decomposition) -> List[LineString]:
     return final_list
 
 
+def vertex_sampler_with_holes(decomposition: Decomposition) -> List[LineString]:
+    """
+    One of the simple samplers that generates cuts that originate at a reflex vertex and end
+    at some vertex of a polygon with holes.
+
+    Args:
+        decomposition (Decomposition): An instance of decomposition.
+
+    Returns:
+        A list of valid cuts.
+    """
+    candidates = vertex_sampler(decomposition)
+    orig_poly = decomposition.orig_polygon
+
+    if len(orig_poly.interiors) == 0:
+        return candidates
+
+    reflex_verts = get_cut_origins_from_holes(decomposition)
+
+    partial_samples: List[Tuple[float, float]] = []
+    for hole_coords in [hole.coords[:-1] for hole in orig_poly.interiors]:
+        reflex_idx = [idx for idx, vertex in enumerate(hole_coords) if vertex in reflex_verts]
+        for start_idx, end_idx in combinations(reflex_idx, 2):
+            partial_samples.append(hole_coords[start_idx:end_idx + 1])
+
+    samples = []
+    for partial_sample in partial_samples:
+        for pre_coord, post_coord in product(orig_poly.exterior.coords[:-1], repeat=2):
+            samples.append([pre_coord, *partial_sample, post_coord])
+
+    for sample in [LineString(coords) for coords in samples]:
+        common_pts = orig_poly.exterior.intersection(sample)
+        if not (common_pts.is_empty or
+                not sample.is_simple or  # Sample shoud not intersect itself.
+                not common_pts.geom_type == 'MultiPoint' or # Intersection should be MultiPoint.
+                len(common_pts) != 2 or # MultiPoint must ONLY have 2 points.
+                not sample.within(orig_poly) or # Split line should be inside cell.
+                any(sample.crosses(hole) for hole in orig_poly.interiors)): # Cut touching holes?
+            candidates.append(sample)
+
+    return candidates
+
+
 def get_cut_origins(decomposition: Decomposition) -> List[Tuple[float, float]]:
     """Find all reflex vertecies in the polygon.
 
@@ -71,6 +122,25 @@ def get_cut_origins(decomposition: Decomposition) -> List[Tuple[float, float]]:
     reflex_verts: List[Tuple[float, float]] = []
     for cell in decomposition.cells:
         for chain in [cell.exterior, *cell.interiors]:
+            points = chain.coords[:-1]
+            for i, _ in enumerate(points):
+                if is_reflex((points[i - 1]), points[i], points[(i + 1) % len(points)]):
+                    reflex_verts.append(points[i])
+    return reflex_verts
+
+
+def get_cut_origins_from_holes(decomposition: Decomposition) -> List[Tuple[float, float]]:
+    """Find all reflex vertecies originating from holes in the polygon.
+
+    Args:
+        decomposition (Decomposition): Instance of a decomposition.
+
+    Returns:
+        List of reflex vertices in the decomposition.
+    """
+    reflex_verts: List[Tuple[float, float]] = []
+    for cell in decomposition.cells:
+        for chain in [*cell.interiors]:
             points = chain.coords[:-1]
             for i, _ in enumerate(points):
                 if is_reflex((points[i - 1]), points[i], points[(i + 1) % len(points)]):
